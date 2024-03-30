96435cef63fdfa4c01ea4f42d143ea6b9d919df0
Prashanth Suseekaran <116962355+Prasha006@users.noreply.github.com>
2024-03-30 19:42:53 +0530

    Add files via upload

---
+++ public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "API_URL": "https://web-scrapping-xobk.onrender.com/"
+};
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const BASE_URL = "https://web-scrapping-xobk.onrender.com/"
+// export const BASE_URL = "https://movie-booking-25w1.onrender.com/"
